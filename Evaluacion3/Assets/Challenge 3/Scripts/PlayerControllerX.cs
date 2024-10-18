2c828b8696e2f142ddc358ca798db596671997aa
Daniel Ruiz <danielruiz2009@hotmail.com>
2024-10-17 20:48:17 -0500

    Add proyect

---
+++ Evaluacion3/Assets/Challenge 3/Scripts/PlayerControllerX.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public bool gameOver;
+
+    public float floatForce;
+    private float gravityModifier = 1.5f;
+    private Rigidbody playerRb;
+
+    public ParticleSystem explosionParticle;
+    public ParticleSystem fireworksParticle;
+
+    private AudioSource playerAudio;
+    public AudioClip moneySound;
+    public AudioClip explodeSound;
+    public AudioClip groundSound;
+
+    // Límites del movimiento en el eje Y
+    public float minY = 0;
+    public float maxY = 12f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Physics.gravity *= gravityModifier;
+        playerAudio = GetComponent<AudioSource>();
+        playerRb = GetComponent<Rigidbody>();
+
+        // Apply a small upward force at the start of the game
+        playerRb.AddForce(Vector3.up * 5, ForceMode.Impulse);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // While space is pressed and player is low enough, float up
+        if (Input.GetKey(KeyCode.Space) && !gameOver)
+        {
+            playerRb.AddForce(Vector3.up * floatForce);
+        }
+        ControlYLimit();
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        // if player collides with bomb, explode and set gameOver to true
+        if (other.gameObject.CompareTag("Bomb"))
+        {
+            explosionParticle.Play();
+            playerAudio.PlayOneShot(explodeSound, 1.0f);
+            gameOver = true;
+            Debug.Log("Game Over!");
+            Destroy(other.gameObject);
+        }
+
+        // if player collides with money, fireworks
+        else if (other.gameObject.CompareTag("Money"))
+        {
+            fireworksParticle.Play();
+            playerAudio.PlayOneShot(moneySound, 1.0f);
+            Destroy(other.gameObject);
+
+        }
+        // if player collides with ground
+        else if (other.gameObject.CompareTag("Floor") && !gameOver)
+        {
+            playerAudio.PlayOneShot(groundSound, 1.0f);
+            playerRb.AddForce(Vector3.up * 12, ForceMode.Impulse);
+        }
+
+    }
+
+    void ControlYLimit()
+    {
+        // Limitar el movimiento en el eje Y
+        if (transform.position.y > maxY)
+        {
+            transform.position = new Vector3(transform.position.x, maxY, transform.position.z);
+            playerRb.velocity = Vector3.zero; // Detener el movimiento hacia arriba
+        }
+        else if (transform.position.y < minY)
+        {
+            transform.position = new Vector3(transform.position.x, minY, transform.position.z);
+            playerRb.velocity = Vector3.zero; // Detener el movimiento hacia abajo
+        }
+    }
+
+}
